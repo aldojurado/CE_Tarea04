430a380dfee1d25a1e5596b7e4270ff040239a6e
Aldo Emilio Jurado Guadalupe <emilio.jurado@ciencias.unam.mx>
2024-02-12 17:07:50 -0600

    Modificaciones a la plantilla

---
+++ src/main/java/CE.java
@@ -0,0 +1,23 @@
+package main.java;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+import javax.swing.JPasswordField;
+
+import java.io.Console;
+import java.io.File;
+import java.io.FileReader;
+
+public class CE {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the CE program!");
+    }
+
+}
