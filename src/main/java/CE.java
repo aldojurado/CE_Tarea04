b23deb3a4e9df2d47f8aa6c91d587340b557f311
Aldo Jurado <emilio.jurado@ciencias.unam.mx>
2024-02-13 19:32:32 -0600

    cambio de PC

--- src/main/java/CE.java
+++ src/main/java/CE.java
@@ -1,7 +1,28 @@
+import java.lang.Math;
+
 public class CE {
 
     public static void main(String[] args) {
-        System.out.println("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec odio. Donec et nunc");
+        int d;
+        double[] rango = new double[2];
+        try {
+            d = Integer.parseInt(args[0]);
+            rango[0] = Double.parseDouble(args[1]);
+            rango[1] = Double.parseDouble(args[2]);
+            Evaluador e = new Evaluador(d, rango);
+            System.out.println("Evaluador creado con éxito");
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        // Pruebas de mínimos
+        Funciones f = new Funciones();
+        double[] valores = { 0, 0 };
+        System.out.println("El resultado es " + f.sumSquare(valores));
+        double[] y = { 1, 0.70710678 };
+        System.out.println("El resultado de dixonPrice " + f.dixonPrice(y));
+        double[] x = { -2.903534, -2.903534 };
+        System.out.print("El resultado de styblinski " + f.StyblinskiTang(x));
+
     }
 
 }
--- src/main/java/Evaluador.java
+++ src/main/java/Evaluador.java
@@ -1,5 +1,11 @@
 
-
 public class Evaluador {
+    int d;
+    double[] rango;
+
+    public Evaluador(int d, double[] rango) {
+        this.d = d;
+        this.rango = rango;
+    }
 
 }
