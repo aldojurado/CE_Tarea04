14f7fd4c7a809d048f7d70169d29c6e178488604
Aldo Emilio Jurado Guadalupe <emilio.jurado@ciencias.unam.mx>
2024-02-26 20:47:31 -0600

    Primera versión de la pregunta 1

--- src/main/java/Funciones.java
+++ src/main/java/Funciones.java
@@ -2,28 +2,54 @@ import java.lang.Math;
 
 public class Funciones {
 
-    public double sumSquare(double[] valores) {
-        double res = 0;
-        for (int i = 0; i < valores.length; i++) {
-            res += (i + 1) * Math.pow(valores[i], 2);
+    public int[] codifica_aux(int n, int nBit) {
+        int[] res = new int[nBit];
+        if (Math.pow(2, nBit) > n) {
+            String bin = Integer.toBinaryString(n);
+            for (int i = 0; i < nBit; i++) {
+                if (i < nBit - bin.length()) {
+                    res[i] = 0;
+                } else {
+                    res[i] = Integer.parseInt(String.valueOf(bin.charAt(i - (nBit - bin.length()))));
+                }
+            }
+        } else {
+            throw new IllegalArgumentException("El número " + n + " no se puede codificar con " + nBit + " bits.");
+        }
+
+        return res;
+    }
+
+    public int[] codifica(double x, int nBits, double a, double b) {
+        int[] res = new int[nBits];
+        double valoresInte = b - a;
+        x = x - a;
+        double pres = valoresInte / (Math.pow(2, nBits) - 1);
+        double valor = x / pres;
+        if (Math.pow(2, nBits) > valor) {
+            String bin = Integer.toBinaryString((int) valor);
+            for (int i = 0; i < nBits; i++) {
+                if (i < nBits - bin.length()) {
+                    res[nBits - 1 - i] = 0;
+                } else {
+                    res[nBits - 1 - i] = Integer.parseInt(String.valueOf(bin.charAt(i - (nBits - bin.length()))));
+                }
+            }
+        } else {
+            throw new IllegalArgumentException(
+                    "El número " + (int) valor + " no se puede codificar con " + nBits + " bits.");
         }
         return res;
     }
 
-    public double StyblinskiTang(double[] x) {
-        double res = 0;
-        for (int i = 0; i < x.length; i++) {
-            res += Math.pow(x[i], 4) - 16 * Math.pow(x[i], 2) + 5 * x[i];
+    public double decodifica(int x_cod[], double a, double b) {
+        double decimal = 0;
+        for (int i = 0; i < x_cod.length; i++) {
+            decimal += x_cod[i] * Math.pow(2, i);
         }
-        return res / 2;
-    }
-
-    public double dixonPrice(double[] x) {
-        double res = Math.pow(x[0] - 1, 2);
-        for (int i = 1; i < x.length; i++) {
-            res += (i + 1) * Math.pow(2 * Math.pow(x[i], 2) - x[i - 1], 2);
-        }
-        return res;
+        double pres = (b - a) / (Math.pow(2, x_cod.length) - 1);
+        decimal = decimal * pres + a;
+        return decimal;
     }
 
 }
