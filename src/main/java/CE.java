77e688c59ef018ea227d92d2875e9f4042e6dca0
Aldo Jurado <emilio.jurado@ciencias.unam.mx>
2024-03-14 14:37:31 -0600

    Revisa enteros

--- src/main/java/CE.java
+++ src/main/java/CE.java
@@ -2,14 +2,35 @@ import java.util.Scanner;
 import java.lang.Math;
 
 public class CE {
+    public static int escaneaNum(int max) {
+        int res = Integer.MAX_VALUE;
+
+        while (res == Integer.MAX_VALUE) {
+            Scanner scanner = new Scanner(System.in);
+            try {
+                int num = scanner.nextInt();
+                if (num < 1 || num > max) {
+                    res = Integer.MAX_VALUE;
+                    System.out.println("Ingrese un índice válido:");
+                } else {
+                    res = num;
+                }
+            } catch (Exception e) {
+                System.out.println("Se debe ingresar un número entero");
+                res = Integer.MAX_VALUE;
+                System.out.println("Ingrese un entero:");
+            }
+
+        }
+        return res;
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Seleccione una opción:");
         System.out.println("1. Problema de coloración");
         System.out.println("2. Problema de codificación");
-        int opcion = scanner.nextInt();
-
+        int opcion = escaneaNum(2);
         if (opcion == 1) {
             System.out.println("Ingrese el nombre del archivo: ");
             String rutaIngresada = scanner.next();
@@ -19,7 +40,7 @@ public class CE {
             System.out.println("Seleccione el tipo de optimización:");
             System.out.println("1. Recocido simulado");
             System.out.println("2. Búsqueda aleatoria");
-            int tipoOptimizacion = scanner.nextInt();
+            int tipoOptimizacion = escaneaNum(2);
             System.out.println("Seleccione la función a evaluar:");
             System.out.println("1. Sphere Function");
             System.out.println("2. Ackley Function");
@@ -29,10 +50,10 @@ public class CE {
             System.out.println("6. Sum Square Function");
             System.out.println("7. Styblinski-Tang Function");
             System.out.println("8. Dixon-Price Function");
-            int numFun = scanner.nextInt();
+            int numFun = escaneaNum(8);
 
             System.out.println("Ingrese la dimensión:");
-            int dimension = scanner.nextInt();
+            int dimension = escaneaNum(50);
 
             OptimizacionCombinatoria metodoOptimizacion = new OptimizacionCombinatoria();
             metodoOptimizacion.ejecutarAlgoritmo(tipoOptimizacion, numFun, dimension);
